79b94cabe8147944c08ee15b8c3ecbfb95282417
myp <850192821@qq.com>
2016-11-14 13:13:06 +0800

    Revert "zhbj"
    This reverts commit f4c990ec4c269c37b6d36aca92b40d32558d849f.

--- Zhbj/src/com/myp/zhbj/global/GlobalConstants.java
+++
@@ -1,7 +0,0 @@
-package com.myp.zhbj.global;
-
-public class GlobalConstants {
-	//public static final String SERVER_URL = "http://zhihuibj.sinaapp.com/zhbj"; //服务器线上地址
-	public static final String SERVER_URL = "http://10.0.2.2:8888/zhbj"; //服务器主域名
-	public static final String CATEGORY_URL = SERVER_URL + "/categories.json"; //分类信息接口
-}
